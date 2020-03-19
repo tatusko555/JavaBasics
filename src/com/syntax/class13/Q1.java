f7c8414a704099ce6203e6004e12aa73d2a0ed16
tatusko555 <tatusko555@gmail.com>
2020-03-18 20:32:01 -0400

    IQ1

--- src/com/syntax/class13/Q1.java
+++ src/com/syntax/class13/Q1.java
@@ -3,8 +3,50 @@ package com.syntax.class13;
 public class Q1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Write a program to swap 2 numbers without a temporary variable? 
+		//Swap  2 strings without a temporary variable?
+
+		int a = 10;
+		int b = 20;
+		
+		a = a+b; // 30
+		b = a-b; // 30-20=10
+		a = a-b;
+		
+		System.out.println("The value of a = "+a);
+		System.out.println("The value of b = "+b);
+		
+		
+		String str1 = "Day";
+		String str2 = "Night";
+		
+		str1 = str1.concat(str2); // DayNight
+		str2 = str1.substring(0, str1.length()-str2.length()); // Day
+		str1 = str1.substring(str2.length()); // Night
+		
+		System.out.println("The value of str1 = "+str1);
+		System.out.println("The value of str2 = "+str2);
+		
+		
+		// Another way
+		
+		String str3 = "Hello";
+		String str4 = "Bye";
+		
+		str3 = str3+str4;
+		str4 = str3.replace(str4, "");
+		str3 = str3.replace(str4, "");
+		
+		System.out.println("The value of str1 = "+str3);
+		System.out.println("The value of str2 = "+str4);
+		
+		
+		
+		
+		
+		
+		
+		
 	}
-
 }
